da8ed2b5d192c210688caceff62a822abc77af28
lpereira <lfpereira@fe.up.pt>
2020-04-08 16:46:10 +0100

    Added StaticStep

--- src/abq/modelling/step.py
+++ src/abq/modelling/step.py
@@ -1,6 +1,6 @@
 '''
 Created on 2020-04-08 14:55:21
-Last modified on 2020-04-08 15:51:11
+Last modified on 2020-04-08 16:53:25
 Python 2.7.16
 v0.1
 
@@ -19,10 +19,15 @@ References
 #%% imports
 
 # abaqus
-from abaqusConstants import (LANCZOS, DEFAULT, SOLVER_DEFAULT)
+from abaqusConstants import (LANCZOS, DEFAULT, SOLVER_DEFAULT, OFF,
+                             AUTOMATIC, NONE, DIRECT, RAMP, FULL_NEWTON,
+                             PROPAGATED, LINEAR)
 
 # standard library
 import abc
+
+
+# TODO: bcs should not be part of a step, but contain step information somehow.
 
 
 #%% abstract classes
@@ -52,6 +57,121 @@ class Step(object):
 
 #%% particular step definition
 
+
+class StaticStep(Step):
+
+    method_name = 'StaticStep'
+
+    def __init__(self, name, previous='Initial', description='',
+                 timePeriod=1., nlgeom=OFF, stabilizationMethod=NONE,
+                 stabilizationMagnitude=2e-4, adiabatic=OFF,
+                 timeIncrementationMethod=AUTOMATIC, maxNumInc=100,
+                 initialInc=None, minInc=None, maxInc=None,
+                 matrixSolver=DIRECT, matrixStorage=SOLVER_DEFAULT,
+                 amplitude=RAMP, extrapolation=LINEAR, fullyPlastic='',
+                 noStop=OFF, maintainAttributes=False,
+                 useLongTermSolution=OFF, solutionTechnique=FULL_NEWTON,
+                 reformKernel=8, convertSDI=PROPAGATED,
+                 adaptiveDampingRatio=0.05, continueDampingFactors=OFF):
+        '''
+        Parameters
+        ----------
+        description : str
+            Step description.
+        timePeriod : float
+            Total time period.
+        nlgeom : bool
+            Whether to allow for geometric nonlinearity.
+        stabilizationMethod : abaqus constant
+            Stabilization type.
+        stabilizationMagnitude : float
+            Damping intensity of the automatic damping algorithm. Ignored if
+            stabilizationMethod=None.
+        adiabatic : bool
+            Whether to perform an adiabatic stress analysis.
+        timeIncrementationMethod : abaqus constant
+        maxNumInc : int
+            Number of incrementations in a step.
+        initalInc : float
+            Initial time increment.
+        minInc : float
+            Minimum tome increment allowed.
+        maxInc : float
+            Maximum time increment allowed.
+        matrixSolver : abaqus constant
+            Type of solver.
+        matrixStorage : abaqus constant
+            Type of matrix storage.
+        amplitude : abaqus constant
+            Amplitude variation for loading magnitudes during the step.
+        extrapolation : abaqus constant
+            Type of extrapolation to use in determining the incremental solution
+            for a nonlinear analysis.
+        fullyPlastic : str
+            Region being monitored for fully plastic behavior.
+        noStop : bool
+            Whether to accept the solution to an increment after the maximum
+            number of interations allowed has been completed, even if the
+            equilibrium tolerances are not satisfied.
+        maintainAttributes : bool
+            Whether to retain attributes from an existing step with the same
+            name.
+        useLongTermSolution : bool
+            Whether to obtain the fully relaxed long-term elastic solution with
+            time-domain viscoelasticity or the long-term elastic-plastic solution
+            for two-layer viscoplasticity.
+        solutionTechnique : abaqus constant
+            Technique used for solving nonlinear equations.
+        reformKernel : int
+            Number of quasi-Newton iterations allowed before the kernel matrix
+            is reformed.
+        convertSDI : abaqus constant
+            Whether to force a new iteration if severe discontinuities occur
+            during an iteration.
+        adaptiveDampingRatio : float
+            Maximum allowable ratio of the stabilization energy to the total
+            strain energy. Ignored if stabilizationMethod=None.
+        continueDampingFactors : bool
+            Whether this step will carry over the damping factors from the
+            results of the preceding general step.
+
+        Notes
+        -----
+        -for further informations see p49-134 of [1].
+        '''
+        # initialize parent
+        Step.__init__(self, name, previous)
+        # computations
+        initialInc = timePeriod if initialInc is None else initialInc
+        minInc = min(initialInc, timePeriod * 1e-5) if minInc is None else minInc
+        maxInc = timePeriod if maxInc is None else maxInc
+        # create args dict
+        self.args = {'description': description,
+                     'timePeriod': timePeriod,
+                     'nlgeom': nlgeom,
+                     'stabilizationMethod': stabilizationMethod,
+                     'stabilizationMagnitude': stabilizationMagnitude,
+                     'adiabatic': adiabatic,
+                     'timeIncrementationMethod': timeIncrementationMethod,
+                     'maxNumInc': maxNumInc,
+                     'initialInc': initialInc,
+                     'minInc': minInc,
+                     'maxInc': maxInc,
+                     'matrixSolver': matrixSolver,
+                     'matrixStorage': matrixStorage,
+                     'amplitude': amplitude,
+                     'extrapolation': extrapolation,
+                     'fullyPlastic': fullyPlastic,
+                     'noStop': noStop,
+                     'maintainAttributes': maintainAttributes,
+                     'useLongTermSolution': useLongTermSolution,
+                     'solutionTechnique': solutionTechnique,
+                     'reformKernel': reformKernel,
+                     'convertSDI': convertSDI,
+                     'adaptiveDampingRatio': adaptiveDampingRatio,
+                     'continueDampingFactors': continueDampingFactors}
+
+
 class BuckleStep(Step):
 
     method_name = 'BuckleStep'
@@ -70,7 +190,7 @@ class BuckleStep(Step):
             Step description.
         eigensolver : abaqus constant
         minEigen : float
-            Minimum eigenvalue of interest.
+            Minimum eigenvalue of interest. Ignored if eigensolver!=LANCZOS.
         maxEigen : float
             Maximum eigenvalue of interest.
         vectors : int
@@ -78,11 +198,12 @@ class BuckleStep(Step):
         maxIterations : int
             Maximum number of iterations.
         blockSize : abaqus constant or int
-            Size of the Lanczos block steps.
+            Size of the Lanczos block steps. Ignored if eigensolver!=LANCZOS.
         maxBlocks : abaqus constant or int
             Maximum number of Lanczos block steps within each Lanczos run.
+            Ignored if eigensolver!=LANCZOS.
         matrixStorage : abaqus constant
-            Type of matrix storage
+            Type of matrix storage.
         maintainAttributes : bool
             Whether to retain attributes from an existing step with the same
             name.
@@ -90,13 +211,11 @@ class BuckleStep(Step):
         Notes
         -----
         -for further informations see p49-10 of [1].
-        -minEigen, blockSize and maxBlocks are ignored if eigensolver!=LANCZOS.
         '''
         # initialize parent
         Step.__init__(self, name, previous)
         # computations
         vectors = min(2 * numEigen, numEigen * 8) if vectors is None else vectors
-
         # create arg dict
         self.args = {'numEigen': numEigen,
                      'description': description,
