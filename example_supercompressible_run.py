68c1179fefa661aaf76a84697c0726669166cf43
lpereira <lfpereira@fe.up.pt>
2020-04-25 19:43:16 +0100

    Added post-processing and dump to run model

--- example_supercompressible_run.py
+++ example_supercompressible_run.py
@@ -1,6 +1,6 @@
 '''
 Created on 2020-04-22 14:43:42
-Last modified on 2020-04-22 22:56:05
+Last modified on 2020-04-25 19:32:14
 Python 3.7.3
 v0.1
 
@@ -18,11 +18,14 @@ import os
 from collections import OrderedDict
 import pickle
 
+# local library
+from f3das.misc.file_handling import verify_existing_name
+
 
 #%% initialization
 
 gui = False
-dir_name = os.path.join(os.getcwd(), 'test')
+dir_name = os.path.join(os.getcwd(), verify_existing_name('test'))
 filename = 'simul.pkl'
 
 # geometry
@@ -32,7 +35,6 @@ mast_pitch = 115.223
 cone_slope = 1.75806e-01
 young_modulus = 3.50000e+03
 shear_modulus = 1.38631e+03
-density = 0.00124
 Ixx = 6.12244e+01
 Iyy = 1.26357e+01
 J = 2.10974e+02
@@ -43,27 +45,26 @@ mode_amplitude = 7.85114e-02
 #%% create and dump data
 
 # create data
-data = {'abstract_model': 'f3das.abaqus.models.supercompressible.SupercompressibleModel',
-        'data': {'n_vertices_polygon': n_vertices_polygon,
-                 'mast_diameter': mast_diameter,
-                 'mast_pitch': mast_pitch,
-                 'cone_slope': cone_slope,
-                 'young_modulus': young_modulus,
-                 'shear_modulus': shear_modulus,
-                 'density': density,
-                 'Ixx': Ixx,
-                 'Iyy': Iyy,
-                 'J': J,
-                 'area': area,
-                 'mode_amplitude': mode_amplitude, },
-        'sim_info': OrderedDict({'SUPERCOMPRESSIBLE_LIN_BUCKLE':
-                                 {'sim_type': 'lin_buckle',
-                                  'job_name': 'Simul_supercompressible_lin_buckle',
-                                  'job_description': ''},
-                                 'SUPERCOMPRESSIBLE_RIKS':
-                                 {'sim_type': 'riks',
-                                  'job_name': 'Simul_supercompressible_riks',
-                                  'job_description': ''}})}
+data = OrderedDict({'abstract_model': 'f3das.abaqus.models.supercompressible.SupercompressibleModel',
+                    'data': {'n_vertices_polygon': n_vertices_polygon,
+                             'mast_diameter': mast_diameter,
+                             'mast_pitch': mast_pitch,
+                             'cone_slope': cone_slope,
+                             'young_modulus': young_modulus,
+                             'shear_modulus': shear_modulus,
+                             'Ixx': Ixx,
+                             'Iyy': Iyy,
+                             'J': J,
+                             'area': area,
+                             'mode_amplitude': mode_amplitude, },
+                    'sim_info': OrderedDict({'SUPERCOMPRESSIBLE_LIN_BUCKLE':
+                                             {'sim_type': 'lin_buckle',
+                                              'job_name': 'Simul_supercompressible_lin_buckle',
+                                              'job_description': ''},
+                                             'SUPERCOMPRESSIBLE_RIKS':
+                                             {'sim_type': 'riks',
+                                                 'job_name': 'Simul_supercompressible_riks',
+                                                 'job_description': ''}})})
 
 # create directory
 os.mkdir(dir_name)
@@ -76,7 +77,7 @@ with open(os.path.join(dir_name, filename), 'wb') as file:
 #%% run abaqus
 
 # create run filename
-run_filename = 'run.py'
+run_filename = verify_existing_name('run.py')
 module_name = 'f3das.abaqus.run.run_model'
 lines = ['import runpy',
          'import os',
--- f3das/abaqus/run/run_model.py
+++ f3das/abaqus/run/run_model.py
@@ -1,6 +1,6 @@
 '''
 Created on 2020-04-22 14:53:01
-Last modified on 2020-04-22 20:38:49
+Last modified on 2020-04-25 16:33:23
 Python 2.7.16
 v0.1
 
@@ -8,6 +8,7 @@ v0.1
 
 Main goal
 ---------
+Run a model sequentially.
 
 
 Notes
@@ -19,9 +20,15 @@ is low).
 
 #%% imports
 
+# abaqus
+from abaqus import session
+
 # standard library
+import os
 import pickle
 import importlib
+from collections import OrderedDict
+import time
 
 # local library
 from .misc import convert_dict_unicode_str
@@ -31,21 +38,66 @@ from .misc import convert_dict_unicode_str
 
 class RunModel(object):
 
-    def __init__(self, abstract_model, data, sim_info):
+    def __init__(self, filename=''):
         '''
+
+        Parameters
+        ----------
+        filename : str
+            Pickle filename. If empty, then assumes there's only one pickle
+            file in the directory.
 
         Notes
         -----
         -assumes the same data is required to instantiate each model of the
         sequence.
-
         '''
-        self.abstract_model = self._import_abstract_model(abstract_model)
-        self.data = data
-        self.sim_info = sim_info
-        self.models = []
+        # read data
+        self.filename = filename
+        data = self._read_data(filename)
+        self.pickle_dict = data
+        # store variables
+        self.abstract_model = self._import_abstract_model(data['abstract_model'])
+        self.data = data['data']
+        self.sim_info = data['sim_info']
+        self.init_time = time.time()
+        # initialize variables
+        self.models = OrderedDict()
+        self.post_processing = OrderedDict()
 
-    def run_models(self):
+    def execute(self):
+
+        # run models
+        self._run_models()
+
+        # post-processing
+        self._perform_post_processing()
+
+        # dump results
+        self._dump_results()
+
+    def _read_data(self, filename):
+
+        # get pickle filename
+        if not filename:
+            for fname in os.listdir(os.getcwd()):
+                if fname.endswith('.pkl'):
+                    self.filename = fname
+                    break
+
+        # read file
+        with open(self.filename, 'rb') as file:
+            data = convert_dict_unicode_str(pickle.load(file))
+
+        return data
+
+    def _import_abstract_model(self, abstract_model):
+
+        module_name, method_name = abstract_model.rsplit('.', 1)
+        module = importlib.import_module(module_name)
+        return getattr(module, method_name)
+
+    def _run_models(self):
 
         for i, (model_name, info) in enumerate(self.sim_info.items()):
 
@@ -54,7 +106,7 @@ class RunModel(object):
             args.update(info)
 
             # instantiate model
-            previous_model = self.models[i - 1] if i else None
+            previous_model = list(self.models.values())[i - 1] if i else None
             model = self.abstract_model(name=model_name,
                                         previous_model=previous_model, **args)
 
@@ -64,30 +116,44 @@ class RunModel(object):
 
             # dump and save model
             model.dump(create_file=False)
-            self.models.append(model)
+            self.models[model_name] = model
 
-        # TODO: make post-processing
-        # TODO: dump results
+    def _perform_post_processing(self):
 
-    def _import_abstract_model(self, abstract_model):
+        for model_name, model in reversed(self.models.items()):
 
-        module_name, method_name = abstract_model.rsplit('.', 1)
-        module = importlib.import_module(module_name)
-        return getattr(module, method_name)
+            # avoid doing again post-processing
+            if model_name in self.post_processing.keys():
+                continue
+
+            # do post-processing of current model
+            odb_name = '%s.odb' % model.job_name
+            odb = session.openOdb(name=odb_name)
+            self.post_processing[model_name] = model.perform_post_processing(odb)
+
+            # save post-processing of previous model (if applicable)
+            if model.previous_model_results is not None and model.previous_model.name not in self.post_processing.keys():
+                self.post_processing[model.previous_model.name] = model.previous_model_results
+        self.post_processing = OrderedDict(reversed(list(self.post_processing.items())))
+
+    def _dump_results(self):
+
+        # results readable outside abaqus
+        self.pickle_dict['post-processing'] = self.post_processing
+        self.pickle_dict['time'] = time.time() - self.init_time
+        with open(self.filename, 'wb') as file:
+            pickle.dump(self.pickle_dict, file, protocol=2)
+
+        # more complete results readable within abaqus
+        self.pickle_dict['models'] = self.models
+        with open('%s_abaqus' % self.filename, 'wb') as file:
+            pickle.dump(self.pickle_dict, file, protocol=2)
 
 
 if __name__ == '__main__':
 
-    # read file
-    # TODO: assume only one pickle file exists
-    filename = 'simul.pkl'
-    with open(filename, 'rb') as file:
-        data = convert_dict_unicode_str(pickle.load(file))
-
-    # create/load run model
-    # TODO: verify if RunModel already exists
-    run_model = RunModel(abstract_model=data['abstract_model'],
-                         data=data['data'], sim_info=data['sim_info'])
+    # create run model
+    run_model = RunModel()
 
     # run models
-    run_model.run_models()
+    run_model.execute()
