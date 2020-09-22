294f7309faefff2ea776520a54c11ff19c74e176
L. F. Pereira <lfpereira@fe.up.pt>
2020-09-22 12:02:50 +0100

    removed python unnecessary files

--- clean_dir.bat
+++
@@ -1,9 +0,0 @@
-@REM @Author: lpereira
-@REM @Date:   2019-09-09 16:02:34
-@REM @Last Modified by:   lpereira
-@REM Modified time: 2020-04-21 22:08:15
-
-@REM see: https://stackoverflow.com/a/53341473
-
-ERASE /S *.rpy *.rpy.* *.pyc *.abq *.com *.dat *.mdl *.msg *.pac *.res *.sel *.stt *.rec *.dmp *.log
-FOR /d /r . %%d IN ("__pycache__") DO @IF EXIST "%%d" rmdir /s /q "%%d"
