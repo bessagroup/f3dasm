596ecf4284cc74ed064aab67ae5d393d76af7b45
L. F. Pereira <lfpereira@fe.up.pt>
2020-09-25 14:32:29 +0100

    finished correction of abaqus run code position

--- f3das/run/abaqus.py
+++ f3das/run/abaqus.py
@@ -1,6 +1,6 @@
 '''
 Created on 2020-04-22 19:50:46
-Last modified on 2020-09-25 14:24:27
+Last modified on 2020-09-25 14:29:58
 
 @author: L. F. Pereira (lfpereira@fe.up.pt)
 '''
@@ -304,38 +304,3 @@ def _get_copy_subroutines(sim_info):
         subroutine_copy_fncs.append(fnc)
 
     return subroutine_copy_fncs
-
-
-def convert_dict_unicode_str(pickled_dict):
-    new_dict = OrderedDict() if type(pickled_dict) is OrderedDict else {}
-    for key, value in pickled_dict.items():
-        value = _set_converter_flow(value)
-        new_dict[str(key)] = value
-
-    return new_dict
-
-
-def convert_iterable_unicode_str(iterable):
-    new_iterable = []
-    for value in iterable:
-        value = _set_converter_flow(value)
-        new_iterable.append(value)
-
-    if type(iterable) is tuple:
-        new_iterable = tuple(new_iterable)
-    elif type(iterable) is set:
-        new_iterable = set(new_iterable)
-
-    return new_iterable
-
-
-def _set_converter_flow(value):
-
-    if type(value) is unicode:
-        value = str(value)
-    elif type(value) in [OrderedDict, dict]:
-        value = convert_dict_unicode_str(value)
-    elif type(value) in [list, tuple, set]:
-        value = convert_iterable_unicode_str(value)
-
-    return value
