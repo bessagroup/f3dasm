3e6522e5bc9997deb7cb6bfa578411e8cf5b2c7d
lpereira95 <lfpereira@fe.up.pt>
2020-11-04 13:20:52 +0000

    refactored material module and added additional functionality

--- f3dasm/abaqus/material/abaqus_materials.py
+++ f3dasm/abaqus/material/abaqus_materials.py
@@ -1,6 +1,6 @@
 '''
 Created on 2020-04-08 12:03:11
-Last modified on 2020-11-02 11:59:58
+Last modified on 2020-11-04 13:17:24
 
 @author: L. F. Pereira (lfpereira@fe.up.pt)
 
@@ -17,13 +17,16 @@ material database.
 # imports
 
 # abaqus
-from abaqusConstants import (ISOTROPIC, ENGINEERING_CONSTANTS, LAMINA)
+from abaqusConstants import (ISOTROPIC, ENGINEERING_CONSTANTS, LAMINA, ORTHOTROPIC)
 
 # standard library
 import abc
 
 
-# abaqus material classe
+# TODO: add user material
+
+
+# abaqus material class
 
 class AbaqusMaterial(object):
 
@@ -93,7 +96,7 @@ class AbaqusMaterial(object):
         material_behaviors = []
 
         # add non-exclusive behaviors
-        for av_material_behavior in OTHER_MATERIAL_BEHAVIORS:
+        for av_material_behavior in GENERAL_MATERIAL_BEHAVIORS:
             for prop in av_material_behavior.required_props:
                 if prop not in self.props.keys():
                     break
@@ -101,15 +104,19 @@ class AbaqusMaterial(object):
                 material_behaviors.append(av_material_behavior())
 
         # add exclusive behaviors
-        for av_material_behavior in ELASTIC_MATERIAL_BEHAVIORS:
-            for prop in av_material_behavior.required_props:
-                if prop not in self.props.keys():
+        for MATERIAL_BEHAVIORS in [ELASTIC_MATERIAL_BEHAVIORS, EXPANSION_MATERIAL_BEHAVIORS]:
+            for av_material_behavior in MATERIAL_BEHAVIORS:
+                for prop in av_material_behavior.required_props:
+                    if prop not in self.props.keys():
+                        break
+                else:
+                    material_behaviors.append(av_material_behavior())
                     break
-            else:
-                material_behaviors.append(av_material_behavior())
-                break
 
         return material_behaviors
+
+    def getAbaqusMaterial(self, model):
+        return model.materials[self.name]
 
 
 # material behavior abstract class
@@ -122,11 +129,64 @@ class MaterialBehavior(object):
     contain the required properties for the behavior.
     '''
 
-    def __init__(self, props=None):
-        self.props = None if props is None else [props[name] for name in self.required_props]
+    def __init__(self, props=None, **kwargs):
+        self.props = None
+        self.opt_props = None
+        self.temperature = None
+        self.kwargs = kwargs
+        if props is not None:
+            self.get_props_from_dict(props)
 
     def get_props_from_dict(self, props):
+
+        # required props
         self.props = [props[name] for name in self.required_props]
+
+        # temperature
+        if 'T' in props.keys():
+            self.temperature = props['T']
+
+        # optional props
+        if hasattr(self, 'optional_props') and props is not None:
+            self.opt_props = {}
+            for name, value in props.items():
+                if name in self.optional_props:
+                    self.opt_props[self.optional_props_map[name]] = value
+            if len(self.opt_props) == 0:
+                self.opt_props = None
+
+    def _get_table(self):
+        if not self.temperature or type(self.props[0]) not in [list, tuple]:
+            return False, (self.props,)
+        else:
+            table = []
+            for i in range(len(self.temperature)):
+                table.append([prop[i] for prop in self.props] + [self.temperature[i]])
+            return True, table
+
+    def create_behavior(self, abaqusMaterial):
+
+        # deal with required properties
+        create_behavior = getattr(abaqusMaterial, self.method_name)
+        kwargs = {}
+        if hasattr(self, 'behavior_type'):
+            kwargs['type'] = self.behavior_type
+        temp_dep, table = self._get_table()
+        if temp_dep:
+            kwargs['temperatureDependency'] = True
+        create_behavior(table=table, **kwargs)
+
+        # deal with additional properties
+        if self.opt_props is not None:
+            self.setValues(abaqusMaterial, **self.opt_props)
+
+        # deal with kwargs
+        if self.kwargs:
+            self.setValues(abaqusMaterial, **self.kwargs)
+
+    def setValues(self, abaqusMaterial, **kwargs):
+        set_values = getattr(abaqusMaterial, self.method_name.lower())
+        set_values.setValues(**kwargs)
 
     @property
     def has_props(self):
@@ -137,59 +197,83 @@ class MaterialBehavior(object):
 
 class Density(MaterialBehavior):
     required_props = ['rho']
+    method_name = 'Density'
 
-    def __init__(self, props=None):
-        MaterialBehavior.__init__(self, props)
-
-    def create_behavior(self, abaqusMaterial):
-        abaqusMaterial.Density(table=((self.props[0], ),))
+    def __init__(self, props=None, **kwargs):
+        super(Density, self).__init__(props, **kwargs)
 
 
 # elastic behavior
 
 class ElasticBehavior(MaterialBehavior):
     __metaclass__ = abc.ABCMeta
+    method_name = 'Elastic'
 
-    def __init__(self, props=None):
-        MaterialBehavior.__init__(self, props)
-
-    def create_behavior(self, abaqusMaterial):
-
-        # define properties
-        abaqusMaterial.Elastic(type=self.elastic_type, table=(self.props,))
+    def __init__(self, props=None, **kwargs):
+        super(ElasticBehavior, self).__init__(props, **kwargs)
 
 
-class IsotropicBehavior(ElasticBehavior):
+class ElasticIsotropicBehavior(ElasticBehavior):
     req_material_orientation = False
-    elastic_type = ISOTROPIC
+    behavior_type = ISOTROPIC
     required_props = ['E', 'nu']
 
-    def __init__(self, props=None):
-        ElasticBehavior.__init__(self, props)
+    def __init__(self, props=None, **kwargs):
+        super(ElasticIsotropicBehavior, self).__init__(props, **kwargs)
 
 
-class LaminaBehavior(ElasticBehavior):
+class ElasticLaminaBehavior(ElasticBehavior):
     req_material_orientation = True
-    elastic_type = LAMINA
+    behavior_type = LAMINA
     required_props = ['E1', 'E2', 'nu12',
                       'G12', 'G13', 'G23']
 
-    def __init__(self, props=None):
-        ElasticBehavior.__init__(self, props)
+    def __init__(self, props=None, **kwargs):
+        super(ElasticLaminaBehavior, self).__init__(props, **kwargs)
 
 
-class EngineeringConstantsBehavior(ElasticBehavior):
+class ElasticEngineeringConstantsBehavior(ElasticBehavior):
     req_material_orientation = True
-    elastic_type = ENGINEERING_CONSTANTS
+    behavior_type = ENGINEERING_CONSTANTS
     required_props = ['E1', 'E2', 'E3',
                       'nu12', 'nu13', 'nu23',
                       'G12', 'G13', 'G23']
 
-    def __init__(self, props=None):
-        ElasticBehavior.__init__(self, props)
+    def __init__(self, props=None, **kwargs):
+        super(ElasticEngineeringConstantsBehavior, self).__init__(props, **kwargs)
 
 
-OTHER_MATERIAL_BEHAVIORS = [Density, ]
+# expansion behavior
 
-ELASTIC_MATERIAL_BEHAVIORS = [EngineeringConstantsBehavior, LaminaBehavior,
-                              IsotropicBehavior, ]  # order by complexity
+class ExpansionBehavior(MaterialBehavior):
+    __metaclass__ = abc.ABCMeta
+    method_name = 'Expansion'
+    optional_props = ['T0']
+    optional_props_map = {'T0': 'zero'}
+
+    def __init__(self, props=None, **kwargs):
+        super(ExpansionBehavior, self).__init__(props, **kwargs)
+
+
+class ExpansionIsotropicBehavior(ExpansionBehavior):
+    required_props = ['alpha']
+    behavior_type = ISOTROPIC
+
+    def __init__(self, props=None, **kwargs):
+        super(ExpansionIsotropicBehavior, self).__init__(props, **kwargs)
+
+
+class ExpansionOrthotropicBehavior(ExpansionBehavior):
+    required_props = ['alpha1', 'alpha2', 'alpha3']
+    behavior_type = ORTHOTROPIC
+
+    def __init__(self, props=None, **kwargs):
+        super(ExpansionOrthotropicBehavior, self).__init__(props, **kwargs)
+
+
+GENERAL_MATERIAL_BEHAVIORS = [Density, ]
+
+ELASTIC_MATERIAL_BEHAVIORS = [ElasticEngineeringConstantsBehavior,
+                              ElasticLaminaBehavior, ElasticIsotropicBehavior, ]  # order by complexity
+
+EXPANSION_MATERIAL_BEHAVIORS = [ExpansionOrthotropicBehavior, ExpansionIsotropicBehavior, ]
--- tests/abaqus/test_materials.py
+++ tests/abaqus/test_materials.py
@@ -1,6 +1,6 @@
 '''
 Created on 2020-04-08 13:57:51
-Last modified on 2020-11-02 12:01:06
+Last modified on 2020-11-04 13:12:14
 
 @author: L. F. Pereira (lfpereira@fe.up.pt)
 '''
@@ -10,14 +10,16 @@ Last modified on 2020-11-02 12:01:06
 
 # abaqus
 from abaqus import mdb, backwardCompatibility
+from abaqusConstants import ON
 
 
 # third-party
 from f3dasm.abaqus.material.abaqus_materials import AbaqusMaterial
-from f3dasm.abaqus.material.abaqus_materials import IsotropicBehavior
-from f3dasm.abaqus.material.abaqus_materials import LaminaBehavior
-from f3dasm.abaqus.material.abaqus_materials import EngineeringConstantsBehavior
+from f3dasm.abaqus.material.abaqus_materials import ElasticIsotropicBehavior
+from f3dasm.abaqus.material.abaqus_materials import ElasticLaminaBehavior
+from f3dasm.abaqus.material.abaqus_materials import ElasticEngineeringConstantsBehavior
 from f3dasm.abaqus.material.abaqus_materials import Density
+from f3dasm.abaqus.material.abaqus_materials import ExpansionIsotropicBehavior
 
 
 # initialization
@@ -40,31 +42,68 @@ if 'Model-1' in mdb.models.keys():
 # define and create materials
 
 # isotropic
-props_isotropic_elastic = {
+props_iso_elastic = {
     'E': 210e9,
     'nu': .32,
     'rho': 7.8e3,
+    'alpha': 4.9E-6,
+    'T0': 120,
 }
 
 # without specifying behaviors
-isotropic_1 = AbaqusMaterial(
-    name='isotropic_no_beh', props=props_isotropic_elastic,
+iso_1 = AbaqusMaterial(
+    name='isotropic_no_beh', props=props_iso_elastic,
     model=model, create_section=True)
 
 # specifying behavior (properties given to behavior)
-iso_beh = IsotropicBehavior(props=props_isotropic_elastic)
-den_beh = Density(props=props_isotropic_elastic)
-isotropic_2 = AbaqusMaterial(
+iso_beh = ElasticIsotropicBehavior(props=props_iso_elastic, noCompression=ON)
+den_beh = Density(props=props_iso_elastic)
+exp_beh = ExpansionIsotropicBehavior(props=props_iso_elastic)
+iso_2 = AbaqusMaterial(
     name='isotropic_beh_no_props',
-    material_behaviors=[iso_beh, den_beh], model=model,
+    material_behaviors=[iso_beh, den_beh, exp_beh], model=model,
     create_section=True)
 
 # specifying behavior (properties given to material)
-iso_beh = IsotropicBehavior()
+iso_beh = ElasticIsotropicBehavior()
 den_beh = Density()
-isotropic_3 = AbaqusMaterial(
-    name='isotropic_beh_props', props=props_isotropic_elastic,
-    material_behaviors=[iso_beh, den_beh], model=model,
+exp_beh = ExpansionIsotropicBehavior()
+iso_3 = AbaqusMaterial(
+    name='isotropic_beh_props', props=props_iso_elastic,
+    material_behaviors=[iso_beh, den_beh, exp_beh], model=model,
+    create_section=True)
+
+
+# isotropic with temperature dependency
+props_iso_elastic_temp = {
+    'T': [10., 20., 30.],
+    'E': [210e9, 200e9, 190e9],
+    'nu': [.32, .32, 0.32],
+    'rho': 7.8e3,
+    'alpha': 4.9E-6,
+    'T0': 120,
+}
+# without specifying behaviors
+iso_temp_1 = AbaqusMaterial(
+    name='isotropic_temp_no_beh', props=props_iso_elastic_temp,
+    model=model, create_section=True)
+
+# specifying behavior (properties given to behavior)
+iso_beh = ElasticIsotropicBehavior(props=props_iso_elastic_temp)
+den_beh = Density(props=props_iso_elastic_temp)
+exp_beh = ExpansionIsotropicBehavior(props=props_iso_elastic_temp)
+iso_temp_2 = AbaqusMaterial(
+    name='isotropic_temp_beh_no_props',
+    material_behaviors=[iso_beh, den_beh, exp_beh], model=model,
+    create_section=True)
+
+# specifying behavior (properties given to material)
+iso_beh = ElasticIsotropicBehavior()
+den_beh = Density()
+exp_beh = ExpansionIsotropicBehavior()
+iso_temp_3 = AbaqusMaterial(
+    name='isotropic_temp_beh_props', props=props_iso_elastic_temp,
+    material_behaviors=[iso_beh, den_beh, exp_beh], model=model,
     create_section=True)
 
 
@@ -87,7 +126,7 @@ eng_const_1 = AbaqusMaterial('eng_const_no_beh', props=props_eng_const,
                              model=model, create_section=False)
 
 # specifying behavior (properties given to behavior)
-eng_const_beh = EngineeringConstantsBehavior(props=props_eng_const)
+eng_const_beh = ElasticEngineeringConstantsBehavior(props=props_eng_const)
 den_beh = Density(props=props_eng_const)
 eng_const_2 = AbaqusMaterial(
     name='eng_const_beh_no_props',
@@ -95,7 +134,7 @@ eng_const_2 = AbaqusMaterial(
     create_section=True)
 
 # specifying behavior (properties given to material)
-eng_const_beh = EngineeringConstantsBehavior()
+eng_const_beh = ElasticEngineeringConstantsBehavior()
 den_beh = Density()
 eng_const_3 = AbaqusMaterial(
     name='eng_const_beh_props', props=props_eng_const,
@@ -115,21 +154,21 @@ props_lamina = {
 }
 
 # without specifying behavior
-lamina_1 = AbaqusMaterial('lamina_no_beh', props=props_lamina,
-                          model=model, create_section=False)
+lam_1 = AbaqusMaterial('lamina_no_beh', props=props_lamina,
+                       model=model, create_section=False)
 
 # specifying behavior (properties given to behavior)
-lam_beh = LaminaBehavior(props=props_lamina)
+lam_beh = ElasticLaminaBehavior(props=props_lamina)
 den_beh = Density(props=props_lamina)
-lamina_2 = AbaqusMaterial(
+lam_2 = AbaqusMaterial(
     name='lamina_beh_no_props',
     material_behaviors=[lam_beh, den_beh], model=model,
     create_section=True)
 
 # specifying behavior (properties given to material)
-lam_beh = LaminaBehavior()
+lam_beh = ElasticLaminaBehavior()
 den_beh = Density()
-lamina_3 = AbaqusMaterial(
+lam_3 = AbaqusMaterial(
     name='lamina_beh_props', props=props_lamina,
     material_behaviors=[lam_beh, den_beh], model=model,
     create_section=True)
