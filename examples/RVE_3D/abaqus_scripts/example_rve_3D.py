7709e7a169a5e0398d1265998b756045c4400214
lpereira95 <lfpereira@fe.up.pt>
2020-11-26 15:46:32 +0000

    further development of 3d example

--- examples/RVE_3D/abaqus_scripts/example_rve_3D.py
+++ examples/RVE_3D/abaqus_scripts/example_rve_3D.py
@@ -1,6 +1,6 @@
 '''
 Created on 2020-10-15 09:30:17
-Last modified on 2020-11-24 13:50:39
+Last modified on 2020-11-26 15:40:17
 
 @author: L. F. Pereira (lfpereira@fe.up.pt))
 '''
@@ -9,22 +9,33 @@ Last modified on 2020-11-24 13:50:39
 
 # abaqus library
 from abaqus import mdb, backwardCompatibility
+from abaqusConstants import OFF
 
 # third-party
 from f3dasm.abaqus.geometry.rve import RVE3D
 # from f3dasm.abaqus.geometry.shapes import Sphere
 from f3dasm.abaqus.geometry.shapes import PeriodicSphere as Sphere
+from f3dasm.abaqus.modelling.step import StaticStep
 from f3dasm.abaqus.material.abaqus_materials import AbaqusMaterial
 from f3dasm.abaqus.material.section import HomogeneousSolidSection
 
 
 # initialization
 
-# backwardCompatibility.setValues(reportDeprecated=False)
+backwardCompatibility.setValues(reportDeprecated=False)
 
 model_name = 'RVE3D'
 job_name = 'Sim_' + model_name
 job_description = ''
+
+# bcs
+eps_11 = 1.
+eps_22 = -0.1
+eps_33 = 1.2
+eps_12 = 0.2
+eps_13 = 0.4
+eps_23 = .5
+epsilon = [eps_11, eps_12, eps_13, eps_22, eps_23, eps_33]
 
 
 # create model
@@ -53,7 +64,7 @@ dims = (1., 1., 1.)
 center = (.5, .5, .5)
 rve = RVE3D(dims=dims, material=matrix_material, center=center)
 bounds = [(c - dim / 2, c + dim / 2) for dim, c in zip(dims, center)]
-rve.add_particle(Sphere(name='PARTICLE_1', center=[1.1, 1.2, 1.], r=0.25,
+rve.add_particle(Sphere(name='PARTICLE_1', center=center, r=0.25,
                         material=fiber_material, bounds=bounds))
 # rve.add_particle(Sphere(name='PARTICLE_2', center=[0., 0., 0.], r=0.25,
 #                         material=None))
@@ -69,8 +80,25 @@ fiber_material.create(model)
 
 rve.create_part(model)
 rve.create_instance(model)
-# success = rve.generate_mesh(simple_trial=True, face_by_closest=False)
-# print('Mesh generated successfully? {}'.format(success))
+success = rve.generate_mesh()
+print('Mesh generated successfully? {}'.format(success))
 
-# # apply boundary conditions
-# rve.apply_pbcs_constraints(model)
+# set step
+step_name = "STATIC_STEP"
+static_step = StaticStep(step_name, initialInc=0.02, timePeriod=1.,
+                         minInc=1e-5, maxInc=0.02)
+static_step.create(model)
+
+# set boundary conditions
+constraints, bcs = rve.bcs.set_bcs(step_name, epsilon,
+                                   green_lagrange_strain=False)
+
+constraints.create(model)
+for bc in bcs:
+    bc.create(model)
+
+
+# create inp
+modelJob = mdb.Job(model=model_name, name=job_name)
+modelJob.writeInput(consistencyChecking=OFF)
+modelJob.submit()
