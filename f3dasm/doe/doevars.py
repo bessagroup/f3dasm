a7a6902ae92fb295620e39c894c2c1522f19aec5
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-10-27 18:40:48 +0200

    extend attributes in doevar dataclass

--- f3dasm/doe/doevars.py
+++ f3dasm/doe/doevars.py
@@ -11,8 +11,14 @@ A dataclass for storing variables (features) during the DoE
 from dataclasses import dataclass, field
 import numpy as np, array
 from .data import DATA
+from typing import Optional
+
+# using Optiona
+# attritube: Optiona[optional-object] = None
 
    # TODO: Decouple data from doe-methods
+
+
 
 @dataclass
 class DoeVars:
@@ -20,15 +26,22 @@ class DoeVars:
 
     sample_size: int
     variables: dict  # boundary conditions 
-    inperfections: any = None
+    shape: str # shape of microstructure, circle, sphere, rectangle, etc.
+    microstructure_size: any # constant, set or range of values
+    material_a: dict = None
+    material_b: dict = None
+    inperfections: any = None 
     feature_names: list = field(init=False)     # Names of features 
     dimensions: int = field(init=False)         # number of dimensions for the feature space of boundary conditions
-    values: array = field(init=False)
-
+    values: array = field(init=False)           # inita value to construct data array
+    
     def __post_init__(self):
         self.feature_names = list(self.variables.keys())
         self.dimensions = len(self.variables)
         self.values = np.zeros((self.sample_size,self.dimensions))    
+
+    #TODO: implement own method to conver to pandas dataframe, use data.py as example
+    
 
     def __str__(self):
 
