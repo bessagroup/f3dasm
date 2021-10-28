43d2f5f6a812c7d92b7bdc866d055ab1b0ee60be
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-10-28 19:55:02 +0200

    implement sampling methods as asbstract class

--- f3dasm/doe/sampling.py
+++ f3dasm/doe/sampling.py
@@ -5,70 +5,98 @@ import numpy
 
 from numpy.core.records import array
 from SALib.sample import sobol_sequence
+from torch._C import Size
 
+@dataclass
+class SamplingMethod(ABC):
+    """Represets a generic sampling method for parmeters with a range of values"""
 
-class Sampling(ABC):
-    """Represets a generic sampling method for the design of experiments"""
-
+    size: int
+    values: dict
+    
     @abstractclassmethod
-    def get_sampling(self, sample_size, range_variables, _type='float') -> array:
+    def compute_sampling(self, aprox='float') -> array:
         """
-        computes N number of samples for the values in the dimensions
+        Computes N number of samples for the values represented as ranges. E.g. [min, max]
+        Values 
         Args:
             sample_size (int): number of samples to be generated
-            range_variables (dict): minumun and maximum values for the range
-            _type (str): type of output for the sample, interger or float. Default is float
-        Returns
+            values (dic): ranges of values to be sample as name:value pairs
+            aprox (int or float): controls if sampled values are aproximated to an integer or to a float. Default is float
+        Returns:
+            sampling results as array 
         """
+    
+    def validate_range(self) -> None:
+        """Checks that elements in values represent value rantes """
+
+        for value in self.values.values():
+            if isinstance(value, list) and len(value) == 2:
+                return True
+            else:
+                raise TypeError("Sampling can only be applied to values representing a range. E.g. [2, 3]")
 
 
-@dataclass
-class SobolSampling(Sampling):
-    """Computes sampling based using a sobol sequence from SALib"""
+    def compute_dimensions(self) -> int:
+        """Computes the number of dimentions for the sampling methon
+        Args:
+            values (dict): values the sampling will be applied to
+        Returns (int): number of elements in values
+        """
+        return len(self.values.keys())
 
-    sample_size: int
-    range_variables: dict
+    
+class Sobol(SamplingMethod):
+    """Computes sampling using a sobol sequence from SALib"""
 
-    def get_sampling(self) -> array:
-        dimensions = get_dimensions(self.range_variables)
-        points = sobol_sequence.sample(self.sample_size, dimensions) 
-        
-        # Stretch the hypercube towards your bounds
-        for i, bound in enumerate(self.range_variables.values()):   
-            points[:,i] = points[:,i] * (bound[1] - bound[0]) + bound[0] 
-        
-        print(points)
+    def compute_sampling(self, aprox='float') -> array:
+        super().validate_range()
+        self.dimensions = super().compute_dimensions()
+        # seeds for the sampling
+        samples = sobol_sequence.sample(self.size, self.dimensions) 
 
-        return points
+        if aprox == 'float':
+            for i, bound in enumerate(self.values.values()):   
+                samples[:,i] = samples[:,i] * (bound[1] - bound[0]) + bound[0] # TODO: values are not stretch to the upper bound
+        else:
+            raise NotImplementedError
+            #TODO: implement case when samples must be integers
+
+        return samples
+       
+
+class Linear(SamplingMethod):
+    """Computes sampling using a linear sequence generator from Numpy"""
+
+    def compute_sampling(self, aprox='float') -> array:
+        super().validate_range()
+        self.dimensions = super().compute_dimensions()
+        samples = numpy.zeros((self.size, self.dimensions))
+    
+        # Streches sampling values toward the bounds given by the original values
+        if aprox == 'float':
+            for i, bound in enumerate(self.values.values()):          
+                samples[:,i] = numpy.linspace(bound[0], bound[1],self.size) 
+        else:
+            raise NotImplementedError
+            #TODO: implement case when samples must be integers
+        return samples
 
 
-@dataclass
-class LinearSampling(Sampling):
-    """Computes sampling based using a linear sequence generator from Numpy"""
+def main():
 
-    sample_size: int
-    range_variables: dict
+    #TODO: write unit test based on this example
 
-    def get_sampling(self) -> array:
-        dimensions = get_dimensions(self.range_variables)
-        points = numpy.zeros((self.sample_size, dimensions))
-        
-        # Stretch the hypercube towards your bounds
-        for i, bound in enumerate(self.range_variables.values()):          
-            points[:,i] = numpy.linspace(bound[0], bound[1],self.sample_size)   
-        return points
+    components= {'F11':[-0.15, 1], 'F12':[-0.1,0.15],'F22':[-0.15, 1]}
+    size = 10
 
+    sobol = Sobol(size, components)
+    samples =sobol.compute_sampling()
+    print(samples)
 
-# Global methonds:
-def get_dimensions(range_variables):
-    """computes the number of diension or Keys in a dictionary
-    Args:
-        range_variables (dict): a dictionary with one or more elemments.
+    linear = Linear(size, components)
+    samples =linear.compute_sampling()
+    print(samples)
 
-    Returns (int): number of keys in dictionary
-    """
-    if isinstance(range_variables, dict):
-        return len(range_variables.keys())
-    else:
-        raise TypeError("Variables for the DoE must be of type dictionary")
-
+if __name__ == "__main__":
+    main()
