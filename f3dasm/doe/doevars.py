a74bc67eba400a71680326aded785bd106e6e042
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-10-28 12:45:32 +0200

    create data classes for DoE

--- f3dasm/doe/doevars.py
+++ f3dasm/doe/doevars.py
@@ -10,39 +10,66 @@ A dataclass for storing variables (features) during the DoE
 
 from dataclasses import dataclass, field
 import numpy as np, array
-from .data import DATA
+from data import DATA
 from typing import Optional
 
 # using Optiona
-# attritube: Optiona[optional-object] = None
+# attritube: Optional[optional-object] = None
 
-   # TODO: Decouple data from doe-methods
 
+@dataclass
+class Material:
+    """represents a material"""
+    parameters: dict  # materials can be represeted a variable list of name:value pairs
+
+
+@dataclass
+class Microsructure:
+    """Represents a microstructue"""
+
+    shape: str
+    size: float
+    material: Material
+
+
+@dataclass
+class Imperfection:
+    """Represents imperfections"""
+
+    #TODO: define the class further, can we define subtypes?
+
+    imperfections: dict # a list parameters defining an imperfection as name:value pairs
+
+
+@dataclass
+class RVE:
+    """Represents an Representative Volume Element"""
+    
+    Lc: float # characteristic length
+    material: Material
+    microstructure: Microsructure
+    dimesionality: int = 2 # e.g. 2D
 
 
 @dataclass
 class DoeVars:
     """Parameters for the design of experiments"""
 
-    sample_size: int
-    variables: dict  # boundary conditions 
-    shape: str # shape of microstructure, circle, sphere, rectangle, etc.
-    microstructure_size: any # constant, set or range of values
-    material_a: dict = None
-    material_b: dict = None
-    inperfections: any = None 
-    feature_names: list = field(init=False)     # Names of features 
-    dimensions: int = field(init=False)         # number of dimensions for the feature space of boundary conditions
-    values: array = field(init=False)           # inita value to construct data array
-    
-    def __post_init__(self):
-        self.feature_names = list(self.variables.keys())
-        self.dimensions = len(self.variables)
-        self.values = np.zeros((self.sample_size,self.dimensions))    
+    boundary_conditions: dict  # boundary conditions 
+    rve: RVE
+    imperfections: Optional[Imperfection] = None
 
-    #TODO: implement own method to conver to pandas dataframe, use data.py as example
+    # feature_names: list = field(init=False)     # Names of features 
+    # dimensions: int = field(init=False)         # number of dimensions for the feature space of boundary conditions
+    # values: array = field(init=False)           # inita value to construct data array
     
+    # def __post_init__(self):
+    #     self.feature_names = list(self.variables.keys())
+    #     self.dimensions = len(self.variables)
+    #     self.values = np.zeros((self.sample_size,self.dimensions))    
 
+    #TODO: implement own method to convert to pandas dataframe, use data.py as example
+    
     def __str__(self):
 
         """ Overwrite print function"""
@@ -51,11 +78,15 @@ class DoeVars:
         print('                       DOE INFO                      ')
         print('-----------------------------------------------------')
         print('\n')
-        print('Module Name          :',self.__name__)
-        print('Method               :',self.method)
+        # print('Module Name          :',self.__name__)
+        # print('Method               :',self.method)
         print('Feature dimension    :',self.dimensions)     
         print('Feature object count :',self.sample_size)
         return '\n'
+
+    # todo: convert values to array
+    # todo: collect names for data colums
+    # pass them on to data.py
 
     def save(self,filename):
 
