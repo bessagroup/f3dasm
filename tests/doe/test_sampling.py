969e26cad2b509b54afe878f7f33926157dac949
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-10-29 12:21:26 +0200

    update unittest for linear sampling method

--- tests/doe/test_sampling.py
+++ tests/doe/test_sampling.py
@@ -46,25 +46,22 @@ class TestSobolSampling(unittest.TestCase):
         self.assertEqual(self.sampling_output.shape[1], 3 )
 
 
-# class TestLinearSampling(unittest.TestCase):
+class TestLinearSampling(unittest.TestCase):
 
-#     def setUp(self) -> None:
-#         """set up test fixtures"""
-#         self.sobol_sampling = LinearSampling(SAMPLE_SIZE, RANGE_VARS)
+    def setUp(self) -> None:
+        """set up test fixtures"""
+        self.linear_sampling = Linear(SAMPLE_SIZE, RANGE_VARS)
+        self.sampling_output = self.linear_sampling.compute_sampling()
 
-#     def test_output_type(self):
-#         """Test that output of sampling method is an array"""
-#         self.assertIsInstance(self.sobol_sampling.get_sampling(), numpy.ndarray)
+    def test_output_type(self):
+        """Test that output of sampling method is an array"""
+        self.assertIsInstance(self.sampling_output, numpy.ndarray)
 
-#     def test_get_dimensions(self):
-#         """test that method returs the correct number of dimensions"""
-#         self.assertEqual(get_dimensions(RANGE_VARS), 3)
+    def test_output_dimentions(self):
+        """test that sampling method produces an array with the correct dimensions"""
+        self.assertEqual(self.sampling_output.shape[0], SAMPLE_SIZE )
+        self.assertEqual(self.sampling_output.shape[1], 3 )
 
-#     def test_get_sampling(self):
-#         """test that sampling method produces an array with the correct dimensions"""
-
-#         self.assertEqual(self.sobol_sampling.get_sampling().shape[0], SAMPLE_SIZE )
-#         self.assertEqual(self.sobol_sampling.get_sampling().shape[1], 3 )
 
 if __name__ == '__main__':
     unittest.main()
