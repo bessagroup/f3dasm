78b55cb45fec22f2ab373b72dfe973be080239b6
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-10-29 16:17:59 +0200

    udpate example of doevars

--- examples/ex_defing_a_doe.py
+++ examples/ex_defing_a_doe.py
@@ -2,15 +2,15 @@
 Example on how to define a DoE
 This example will be moved to the documentation
 """
-from f3dasm.doe.doevars import CilinderMicrostructure, Material, CircleMicrostructure, REV, DoeVars
+from f3dasm.doe.doevars import CilinderMicrostructure, DoeVars, Material, CircleMicrostructure, REV, DoE
 
 
 # define strain components
 components= {'F11':[-0.15, 1], 'F12':[-0.1,0.15],'F22':[-0.15, 1]}
 
 # define material for RVE and microstructure
-mat1 = Material({'param1': 1, 'param2': 2})
-mat2 = Material({'param1': 3, 'param2': 4, 'param3': 'value3'})
+mat1 = Material({'name': 'STEEL', 'param1': 1, 'param2': 2})
+mat2 = Material({'name': 'CARBON', 'param1': 3, 'param2': 4, 'param3': 'value3'})
 
 # create a microstructure 
 #circle
@@ -23,5 +23,10 @@ micro2 = CilinderMicrostructure(material=mat1, diameter=0.3, length=1.0)
 rev = REV(Lc=4,material=mat1, microstructure=micro, dimesionality=2)
 doe = DoeVars(boundary_conditions=components, rev=rev)
 
-print(rev)
+print('DoEVars definition:')
 print(doe)
+
+print('DoEVars summary information:')
+print(doe.info())
+
+
