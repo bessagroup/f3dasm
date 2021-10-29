095213257a1b8ecc7b0d513beb6af3ee2bdc5327
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-10-29 16:52:09 +0200

    update material definition for example

--- examples/ex_defing_a_doe.py
+++ examples/ex_defing_a_doe.py
@@ -2,15 +2,25 @@
 Example on how to define a DoE
 This example will be moved to the documentation
 """
-from f3dasm.doe.doevars import CilinderMicrostructure, DoeVars, Material, CircleMicrostructure, REV, DoE
+from f3dasm.doe.doevars import CilinderMicrostructure, DoeVars, Material, CircleMicrostructure, REV, DoeVars
 
 
 # define strain components
 components= {'F11':[-0.15, 1], 'F12':[-0.1,0.15],'F22':[-0.15, 1]}
 
 # define material for RVE and microstructure
-mat1 = Material({'name': 'STEEL', 'param1': 1, 'param2': 2})
-mat2 = Material({'name': 'CARBON', 'param1': 3, 'param2': 4, 'param3': 'value3'})
+# material are must be defined as an list of 'elements', which 
+# are declared as dictionarinaries containing a 'name' and a list of parameters
+# ('params') also declared as dictionaries. However, the names and number of elements 
+# and parameters can vary
+mat1 = Material({'elements': [ {'name': 'STEEL', 'params': {'param1': 1, 'param2': 2}},
+                    {'name': 'CARBON', 'params': {'param1': 3, 'param2': 4, 'param3': 'value3'} }
+                    ]
+                })
+
+mat2 = Material({'elements': [{'name': 'CARBON', 'params': {'param1': 3, 'param2': 4, 'param3': 'value3'}}
+                    ]
+                })
 
 # create a microstructure 
 #circle
