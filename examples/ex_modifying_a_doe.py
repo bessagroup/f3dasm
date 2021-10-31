38115feaf0343f53d6ee64078d0dbeaa82697faf
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-10-31 13:48:05 +0100

    add example on how to modify doe definitions

---
+++ examples/ex_modifying_a_doe.py
@@ -0,0 +1,58 @@
+"""
+Example on how to modify  the parameters (definition) of a DoE
+This example will be moved to the documentation
+"""
+from f3dasm.doe.doevars import CilinderMicrostructure, DoeVars, Material, CircleMicrostructure, REV, DoeVars
+
+
+# -------------------------------------------
+# Create DoE with original parameters
+# -------------------------------------------
+# define strain components
+components= {'F11':[-0.15, 1], 'F12':[-0.1,0.15],'F22':[-0.15, 1]}
+
+# define material for RVE and microstructure
+# material are must be defined as an list of 'elements', which 
+# are declared as dictionarinaries containing a 'name' and a list of parameters
+# ('params') also declared as dictionaries. However, the names and number of elements 
+# and parameters can vary
+mat1 = Material({'elements': [ {'name': 'STEEL', 'params': {'param1': 1, 'param2': 2}},
+                    {'name': 'CARBON', 'params': {'param1': 3, 'param2': 4, 'param3': 'value3'} }
+                    ]
+                })
+
+mat2 = Material({'elements': [{'name': 'CARBON', 'params': {'param1': 3, 'param2': 4, 'param3': 'value3'}}
+                    ]
+                })
+
+mat3 = Material({'elements': [{'name': 'BRONZE', 'params': {'param1': 5, 'param2': 6, 'param4': 'value4'}}
+                    ]
+                })
+
+# create a microstructure 
+#circle
+micro = CircleMicrostructure(material=mat2, diameter=0.3)
+
+# create RVE
+rev = REV(Lc=4,material=mat1, microstructure=micro, dimesionality=2)
+doe = DoeVars(boundary_conditions=components, rev=rev)
+
+print('DoEVars Original parameters:')
+print(doe)
+
+
+# -------------------------------------------
+# Modifying DoE  parameters
+# -------------------------------------------
+
+#.  1 Change material of microstructure
+doe.rev.microstructure.material = mat3
+print('\n', 'Changed Material of microstructure:')
+print('\n', doe)
+
+#.  2 Change diameter of geometry of microstructure
+
+new_micro = CilinderMicrostructure(material=mat1, diameter=0.3, length=1.0)
+doe.rev.microstructure = new_micro
+print('\n', 'Changed microstructure geometry:')
+print('\n', doe)
