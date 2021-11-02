01293236b497f117c358498c60a63123f158274b
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-11-02 22:45:33 +0100

    clean the code up

--- f3dasm/doe/sampling.py
+++ f3dasm/doe/sampling.py
@@ -58,21 +58,18 @@ class SamplingMethod(ABC):
                     raise TypeError("Input don't contain a valid range of vlaues. E.g. [2.1, 3]")
         else: # when input_type is list
             if len(self.values) == 2 and isinstance(self.values[0], (int, float)) and isinstance(self.values[1], (int, float)):
-                print('input is list')
                 return True
             else:
-                print(self.values)
                 raise TypeError("Range of values contains one of more values that are not numeric.")
                 
 
     def compute_dimensions(self) -> int:
-        """Computes the number of dimentions for the sampling methon
+        """Computes the number of dimentions for the sampling method based on input data type.
         Args:
-            values (dict): values the sampling will be applied to
-        Returns (int): number of elements in values
+            values (dict, list): values the sampling will be applied to.
+        Returns (int): number of dimentions in the 'values' attributes.
         """
         if isinstance(self.check_input_type(), dict):
-            print('type', type(self.check_input_type))
             return len(self.values.keys())
         else: # for the case of a list, dimentions must always be 1
             return 1
@@ -85,7 +82,6 @@ class Sobol(SamplingMethod):
     def compute_sampling(self, aprox='float') -> array:
         super().validate_range()
         self.dimensions = super().compute_dimensions()
-        print(self.dimensions)
 
         #----------------------------------------------------------
         # Implementation of Sampling Method
@@ -123,7 +119,6 @@ class Linear(SamplingMethod):
             for i, bound in enumerate(self.values.values()): 
                 samples[:,i] = numpy.linspace(bound[0], bound[1],self.size)
         elif aprox == 'float' and isinstance(self.check_input_type(), list):
-            print('input type is list')
             samples = numpy.linspace(self.values[0], self.values[1],self.size)
             # samples = samples * (self.values[1] - self.values[0]) + self.values[0]
         else:
@@ -145,15 +140,15 @@ def main():
     # # print(sobol1.check_input_type())
     # print(samples)
 
-    # print('List Case, dimentions=1')
-    # var_range = [5, 10]
-    # sobol2 = Sobol(size, var_range)
-    # samples = sobol2.compute_sampling()
-    # print(samples)
+    print('List Case, dimentions=1')
+    var_range = [5, 10]
+    sobol2 = Sobol(size, var_range)
+    samples = sobol2.compute_sampling()
+    print(samples)
 
     linear = Linear(size, components)
     samples =linear.compute_sampling()
-    print(samples)
+    # print(samples)
 
     var_range = [5, 10]
     linear2 = Linear(size, var_range)
