adf463f37468cb8da0e66b56ea597ee3ce6b1536
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-11-10 11:57:04 +0100

    implement tree-data structure in doevars.py

--- f3dasm/doe/doevars.py
+++ f3dasm/doe/doevars.py
@@ -14,6 +14,7 @@ import numpy as np, array
 from typing import Optional
 from abc import ABC
 import pandas as pd
+import pprint
 
 
 ## working example:
@@ -40,53 +41,57 @@ d = {'F11':[-0.15, 1],
      }
 
 
+def print_variables(dictionary:dict):
+    """Print the top level elements in a dictionary"""
 
+    keys = dictionary.keys()
+    for k in keys:
+        print(k,':', dictionary[k])
+
+    return None
+
+
+@dataclass
 class DoeVars:
     """Parameters for the design of experiments"""
 
-    boundary_conditions: dict  # boundary conditions 
-    rev: REV
-    imperfections: Optional[Imperfection] = None
+    variables: dict
 
     def info(self):
 
         """ Overwrite print function"""
 
+        
         print('-----------------------------------------------------')
-        print('                       DOE INFO                      ')
+        print('                       DOE VARIABLES                     ')
         print('-----------------------------------------------------')
+        print_variables(self.variables)
         print('\n')
-        print('Boundary conditions:',self.boundary_conditions)
-        print('REV dimensions:',self.rev.dimesionality)
-        print('REV Lc:',self.rev.Lc)
-        print('REV material:',self.rev.material.parameters)
-        print('Microstructure shape:',self.rev.microstructure.shape)
-        print('Microstructure material:',self.rev.microstructure.material.parameters)
-        print('Imperfections:',self.imperfections)
-        return '\n'
+        return None
 
     # todo: convert values to array
     # todo: collect names for data colums
     # pass them on to data.py
     #TODO: implement own method to convert to pandas dataframe, use data.py as example
     
-    def pandas_df(self, max_level=None):
+    def pandas_df(self, max_level=0):
         """
-        Converts DoeVars into a normilized flat table.
+        Converts DoE variables into a normilized data frame. By default only the elements on the top level are serialized.
         Args:
             max_level: Max number of levels(depth of dict) to normalize. if None, normalizes all levels.
         Returns:
             pandas dataframe
         """
         pd.set_option('display.max_columns', None) # show all colums in the dataframe
-        normalized_dataframe = pd.json_normalize(asdict(self), max_level=max_level)
+        normalized_dataframe = pd.json_normalize(self.variables, max_level=max_level)
+
         return normalized_dataframe
 
     def as_dict(self):
         """
         Convert DoeVars into a nested dictionary
         """
-        return asdict(self)
+        return self.variables
 
 
     def save(self,filename):
@@ -104,36 +109,56 @@ class DoeVars:
         data_frame.to_pickle(filename)
 
 
-
-
 def main():
 
     from dataclasses import asdict
     import json
     import pandas as pd
 
-    components= {'F11':[-0.15, 1], 'F12':[-0.1,0.15],'F22':[-0.15, 1]}
-    mat1 = Material({'param1': 1, 'param2': 2})
-    mat2 = Material({'elements': [{'name': 'CARBON', 'params': {'param1': 3, 'param2': 4, 'param3': 'value3'}}
-                    ]
-                })
-    micro = CircleMicrostructure(material=mat2, diameter=[0.3, 0.5])
-    rev = REV(Lc=4,material=mat1, microstructure=micro, dimesionality=2)
-    doe = DoeVars(boundary_conditions=components, rev=rev)
+    d = {'F11':[-0.15, 1], 
+    'F12':[-0.1,0.15],
+    'F22':[-0.15, 1], 
+    'radius': [0.3, 5],  
+    'material1': {'STEEL': {
+                    'E': [0,100], 
+                    'u': {0.1, 0.2, 0.3} 
+                        }, 
+                'CARBON': {
+                    'E': 5, 
+                    'u': 0.5, 
+                    's': 0.1 
+                    } 
+                },
+    'material2': {
+                'CARBON': {
+                    'x': 2
+                    } 
+                },
+     }
+
+
+    doe = DoeVars(variables=d)
 
     print(doe)
+
+    doe.info()
+    # print(doe.as_dict())
+    print(doe.pandas_df())
+    doe.save('/home/manuel/Documents/development/f3dasm/F3DASM-1/examples/data-frame.pkl')
+    
+
 
     # print(doe.info())
     # print(asdict(doe))
     # print(json.dumps(asdict(doe)))
      
-    pd.set_option('display.max_columns', None)
-    norm_pd = pd.json_normalize(asdict(doe), max_level=1)
-    df = norm_pd.rename(columns= {"rev.Lc": "Lc"}, errors="raise")
-    # print(df)
-    print(doe.pandas_df())
+    # pd.set_option('display.max_columns', None)
+    # norm_pd = pd.json_normalize(asdict(doe), max_level=1)
+    # df = norm_pd.rename(columns= {"rev.Lc": "Lc"}, errors="raise")
+    # # print(df)
+    # print(doe.pandas_df())
 
-    print(doe.as_dict())
+    # print(doe.as_dict())
 
 
 
