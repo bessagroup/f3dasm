41eb4ca397f7631c822ee9d01333e83cea0ef8c6
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-11-10 12:01:09 +0100

    delete unittest s for microstructures and materials

--- tests/doe/test_doevars.py
+++ tests/doe/test_doevars.py
@@ -5,7 +5,7 @@
 ##############################################################
 
 import unittest
-from f3dasm.doe.doevars import DoeVars, Imperfection, Material, CircleMicrostructure, CilinderMicrostructure, REV, BaseMicrosructure
+from f3dasm.doe.doevars import DoeVars, REV
 import pandas as pd
 
 B_CONDITIONS = {'F11':[-0.15, 1], 'F12':[-0.1,0.15],'F22':[-0.15, 1]} 
@@ -23,26 +23,6 @@ IMPERFFECTIONS= {'imp1': 1.2, 'imp2': {1,2,3}}
 LC = 4
 DIAMETER= 0.3
 LENGTH = 1
-
-
-class TestMaterial(unittest.TestCase):
-    """Write relevant tests"""
-    pass
-    
-
-class TestMicrostructures(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.micro_circle = CircleMicrostructure(MATERIAL_A, DIAMETER)
-        self.micro_cilinder = CilinderMicrostructure(MATERIAL_A, DIAMETER, LENGTH)
-        
-    def test_circle_inheritance(self) -> None:
-        """Test that  CircleMicrostruture inherits from BaseMicrostructure"""
-        self.assertIsInstance(self.micro_circle, BaseMicrosructure)
-
-    def test_cilinder_inheritance(self) -> None:
-        """Test that  CilinderMicrostruture inherits from BaseMicrostructure"""
-        self.assertIsInstance(self.micro_cilinder, BaseMicrosructure)
 
 
 class TestRVE(unittest.TestCase):
