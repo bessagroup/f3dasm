5f2ca1e13ca933e5913291ce144ded8747188224
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-11-10 11:57:53 +0100

    update example for doe definition using tree data structure

--- examples/ex_defing_a_doe.py
+++ examples/ex_defing_a_doe.py
@@ -2,38 +2,21 @@
 Example on how to define a DoE
 This example will be moved to the documentation
 """
-from f3dasm.doe.doevars import CilinderMicrostructure, DoeVars, Material, CircleMicrostructure, REV, DoeVars
-from dataclasses import asdict
+from f3dasm.doe.doevars import  DoeVars
 
 
-# define strain components
-components= {'F11':[-0.15, 1], 'F12':[-0.1,0.15],'F22':[-0.15, 1]}
+# define variables for the DoE as a dictionary, for example
+vars = {
+    'F11':[-0.15, 1], 
+    'F12':[-0.1,0.15],
+    'F22':[-0.15, 1], 
+    'radius': [0.3, 5],  
+    'material1': {'STEEL': {'E': [0,100], 'u': {0.1, 0.2, 0.3} }, 
+                'CARBON': {'E': 5, 'u': 0.5, 's': 0.1 } },
+    'material2': { 'CARBON': {'x': 2} },
+    }
 
-# define material for RVE and microstructure
-# material  must be defined as an list of 'elements', which 
-# are declared as dictionarinaries containing a 'name' and a list of parameters
-# ('params') also declared as dictionaries. However, the names and number of elements 
-# and parameters can vary
-mat1 = Material({'elements': [ {'name': 'STEEL', 'params': {'param1': 1, 'param2': 2}},
-                    {'name': 'CARBON', 'params': {'param1': 3, 'param2': 4, 'param3': 'value3'} }
-                    ]
-                })
-
-mat2 = Material({'elements': [{'name': 'CARBON', 'params': {'param1': 3, 'param2': 4, 'param3': 'value3'}}
-                    ]
-                })
-
-
-# create a microstructure 
-#circle
-micro = CircleMicrostructure(material=mat2, diameter=0.3)
-
-#cilinder
-micro2 = CilinderMicrostructure(material=mat1, diameter=0.3, length=1.0)
-
-# create RVE
-rev = REV(Lc=4,material=mat1, microstructure=micro, dimesionality=2)
-doe = DoeVars(boundary_conditions=components, rev=rev)
+doe = DoeVars(vars)
 
 print('DoEVars definition:')
 print(doe)
@@ -44,7 +27,5 @@ print(doe.info())
 print('\n DoEVars as nested dictionary:')
 print(doe.as_dict())
 
-print('\n DoEVars as normilized pandas dataframe:')
+print('\n DoEVars as top-level normilized pandas dataframe:')
 print(doe.pandas_df(max_level=0))
-
-
