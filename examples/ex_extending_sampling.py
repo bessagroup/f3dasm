4faf5cf1071ebdcc952b9bc38994053fd87003e4
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-11-11 10:12:50 +0100

    update example of extending sampling method

--- examples/ex_extending_sampling.py
+++ examples/ex_extending_sampling.py
@@ -5,41 +5,35 @@ This example will be moved to the documentation
 
 from f3dasm.doe.sampling import SamplingMethod
 from numpy.core.records import array
+import numpy as np
 
 class MyCustomeSampling(SamplingMethod):
 
 # write custome sampling method to the 'compute_sampling' function
     def compute_sampling(self, aprox='float') -> array:
-        # validate values represente ranges, this is a list with two elements
-        super().validate_range()
-
-        # compute dimentions of the input data if required by your sampling method
-        self.dimensions = super().compute_dimensions()
         
         #----------------------------------------------------------
         # Implementation of Sampling Method
         # ----------------------------------------------------------
-        # 1. seeds for the sampling:
+        # 1. seeds for the sampling. Example:
+
+        samples = np.random.randint(10, size=(self.size, self.dimensions )) # output must be an Numpy array
  
         # 2. cases for streaching values based on input data types, if required:
-        # 2.1. Case for inputs as dictionary:
+        # 2.1. Case for  output as float, aprox = 'float'
 
-        # 2.1. Case for inputs as list:
-
-        # 2.2. Case for X:
+        # 2.1. Case for output as integer, aprox = 'int' 
     
-        samples = ['my sampling results']  # output must be an Numpy array
-
         # ----------------------------------------------------------
 
         return samples
 
 # Use:
 
-# define strain components
+# define DoE parameters
 values= {'F11':[-0.15, 1], 'F12':[-0.1,0.15],'F22':[-0.15, 1]}
-sampling_size = 5
+sampling_size = 10
 sampling = MyCustomeSampling(sampling_size, values)
-print('Sampling results:', sampling.compute_sampling())
+print('Sampling results: \n', sampling.compute_sampling())
 
 
