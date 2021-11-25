08353aa5c3c16731638e1700404592025774d4c5
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-11-25 02:44:59 +0100

    add sample method

--- f3dasm/doe/doevars.py
+++ f3dasm/doe/doevars.py
@@ -6,6 +6,7 @@
 
 from dataclasses import dataclass, field
 import pandas as pd
+from f3dasm.doe.sampling import SamplingMethod, SalibSobol
 
 def print_variables(dictionary:dict):
     """Print the top level elements in a dictionary"""
@@ -16,6 +17,22 @@ def print_variables(dictionary:dict):
 
     return None
 
+def find_sampling_vars(doe_vars: dict):
+    """Find names of DoeVars that contain a definiton of a sampling method
+    Args:
+        doe_vars (dict): variables defining a design of experiment
+    Returns:
+        list of names
+    """
+    # expand dictionary
+    df = pd.json_normalize(doe_vars)
+    vars = df.to_dict(orient='records')[0]
+
+    elements_with_functions = [] # list of names
+    [ elements_with_functions.append(var) for var in vars.keys() if isinstance(vars[var], SamplingMethod) ]
+
+    return elements_with_functions
+
 
 @dataclass
 class DoeVars:
@@ -23,7 +40,9 @@ class DoeVars:
 
     variables: dict
     sampling_vars: list = field(init=False)
-    
+
+    def __post_init__(self):
+        self.sampling_vars = find_sampling_vars(self.variables)
 
     def info(self):
 
@@ -37,7 +56,7 @@ class DoeVars:
         print('\n')
         return None
     
-    def as_dataframe(self, max_level=0):
+    def as_dataframe(self, max_level: int =0):
         """
         Converts DoE variables into a normilized Pandas data frame. By default only the elements on the top level are serialized.
         Args:
@@ -56,6 +75,21 @@ class DoeVars:
         """
         return self.variables
 
+    def sample(self):
+        """Apply sampling method to sampling variables"""
+
+        for var in self.sampling_vars:
+            inner_vars = var.split('.') 
+            if len(inner_vars) == 1:
+                self.variables[var] = self.variables[var].compute_sampling()
+            elif len(inner_vars) == 2:
+                self.variables[inner_vars[0]][inner_vars[1]] = self.variables[inner_vars[0]][inner_vars[1]].compute_sampling()
+            elif len(inner_vars) == 3:
+                self.variables[inner_vars[0]][inner_vars[1]][inner_vars[2]] = self.variables[inner_vars[0]][inner_vars[1]][inner_vars[2]].compute_sampling()
+            else:
+                raise SyntaxError("DoeVars definition contains too many nested elements. A max of 3 is allowed")
+        
+        return self.variables
 
     def save(self,filename):
 
@@ -69,3 +103,25 @@ class DoeVars:
 
         data_frame = self.pandas_df()       # f3dasm data structure, numpy array
         data_frame.to_pickle(filename)
+
+def main():
+    vars = {'Fs': SalibSobol(5, {'F11':[-0.15, 1], 'F12':[-0.1,0.15], 'F22':[-0.2, 1]}),
+            'R': SalibSobol(2, {'radius': [0.3, 0.5]}),
+            'particle': { 
+                'name': 'NeoHookean',
+                'E': SalibSobol(5 , {'radius': [0.3, 0.5]}), 
+                'nu': 0.4 
+                } ,
+            'matrix': {  
+                'name': 'SaintVenant',  
+                'E': [5, 200],
+                'nu': 0.3
+                }
+            }
+
+    doe = DoeVars(vars)
+    print(doe.sampling_vars)
+    print(doe.sample())
+
+if __name__ == "__main__":
+    main()
