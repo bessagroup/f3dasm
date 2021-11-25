e23a6df86295c2af45fed070c8a5d454c370fdb1
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-11-25 06:01:40 +0100

    update example defining doe

--- examples/ex_defing_a_doe.py
+++ examples/ex_defing_a_doe.py
@@ -3,18 +3,22 @@ Example on how to define a DoE
 This example will be moved to the documentation
 """
 from f3dasm.doe.doevars import  DoeVars
-
+from f3dasm.doe.sampling import SalibSobol
 
 # define variables for the DoE as a dictionary, for example
-vars = {
-    'F11':[-0.15, 1], 
-    'F12':[-0.1,0.15],
-    'F22':[-0.15, 1], 
-    'radius': [0.3, 5],  
-    'material1': {'STEEL': {'E': [0,100], 'u': {0.1, 0.2, 0.3} }, 
-                'CARBON': {'E': 5, 'u': 0.5, 's': 0.1 } },
-    'material2': { 'CARBON': {'x': 2} },
-    }
+vars = {'Fs': SalibSobol(5, {'F11':[-0.15, 1], 'F12':[-0.1,0.15], 'F22':[-0.2, 1]}),
+            'R': SalibSobol(3, {'radius': [0.3, 0.5]}),
+            'particle': { 
+                'name': 'NeoHookean',
+                'E': [0.3, 0.5], 
+                'nu': 0.4 
+                } ,
+            'matrix': {  
+                'name': 'SaintVenant',  
+                'E': [5, 200, 300],
+                'nu': 0.3
+                }
+            }
 
 doe = DoeVars(vars)
 
@@ -24,8 +28,8 @@ print(doe)
 print('\n DoEVars summary information:')
 print(doe.info())
 
-print('\n DoEVars as nested dictionary:')
-print(doe.as_dict())
+# Compute sampling and combinations
+doe.do_sampling()
 
-print('\n DoEVars as top-level normilized pandas dataframe:')
-print(doe.pandas_df())
+print('\n Pandas dataframe with compbined-sampled values:')
+print(doe.data)
