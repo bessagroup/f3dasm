c3c8f9ad2f152380d13c99cbf06c4a2a3f50df31
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2022-06-08 13:29:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # %%
 
+import numpy as np
 from f3dasm.src.designofexperiments import DesignOfExperiments
 from f3dasm.sampling.sobolsequence import SobolSequencing
 from f3dasm.sampling.randomuniform import RandomUniform
 from f3dasm.sampling.latinhypercube import LatinHypercube
-
 
 # %%
 
@@ -15,6 +15,8 @@ ran = RandomUniform(doe)
 # %%
 dim = 4
 samp = 5
+np.random.seed(42)
+
 sob.sample(samp, dim)
 lhs.sample(samp, dim)
 ran.sample(samp, dim)
