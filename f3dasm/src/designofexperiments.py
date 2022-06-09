b03f7fe4d9ac2994256edd25d89d4045a7632801
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2022-06-09 12:49:29 +0200

    Update designofexperiments.py

--- f3dasm/src/designofexperiments.py
+++ f3dasm/src/designofexperiments.py
@@ -2,26 +2,33 @@
 from dataclasses import dataclass, field
 from typing import List
 
-from f3dasm.src.space import ConstraintInterface, SpaceInterface
-
-
-@dataclass
-class DesignOfExperiments:
-    """Design of Experiments"""
-
-    pass
+from f3dasm.src.space import (
+    ConstraintInterface,
+    ContinuousSpace,
+    DiscreteSpace,
+    SpaceInterface,
+)
 
 
 @dataclass
 class DoE:
 
     space: List[SpaceInterface] = field(default_factory=list)
-    constraints: List[ConstraintInterface] = field(default_factory=list)
 
-    def add_space(self, space: SpaceInterface) -> None:
+    def addSpace(self, space: SpaceInterface) -> None:
         self.space.append(space)
         return
 
-    def add_constraint(self, constraint: ConstraintInterface) -> None:
-        self.constraints.append(constraint)
-        return
+    def getContinuousParameters(self):
+        return [
+            parameter
+            for parameter in self.space
+            if isinstance(parameter, ContinuousSpace)
+        ]
+
+    def getDiscreteParameters(self):
+        return [
+            parameter
+            for parameter in self.space
+            if isinstance(parameter, DiscreteSpace)
+        ]
