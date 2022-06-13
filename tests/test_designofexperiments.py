483f7d54c6c0a36e558370f2ac4170c25051f25e
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2022-06-13 12:25:28 +0200

    fixed tests

--- tests/test_designofexperiments.py
+++ tests/test_designofexperiments.py
@@ -1,13 +1,75 @@
-# -*- coding: utf-8 -*-
-
-# from .context import f3dasm
-
+from pandas import Categorical
 import pytest
-from f3dasm.src.designofexperiments import ContinuousParameter
+from f3dasm.src.designofexperiments import DoE
+from f3dasm.src.space import CategoricalSpace, ContinuousSpace, DiscreteSpace
 
-def test_doe():
-    with pytest.raises(ValueError):
-        a = ContinuousParameter('test', 0.3, 0.2)
 
-if __name__ == '__main__':
+def test_empty_space_doe():
+    doe = DoE()
+    empty_list = []
+    assert doe.space == empty_list
+
+
+def test_correct_doe():
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    designspace = [x1, x2, x3, x4]
+
+    doe = DoE(space=designspace)
+
+
+def test_get_continuous_parameters():
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = DiscreteSpace(name="x5", lower_bound=2, upper_bound=3)
+    designspace = [x1, x2, x3, x4, x5]
+
+    doe = DoE(space=designspace)
+    assert doe.getContinuousParameters() == [x1, x3]
+
+
+def test_get_discrete_parameters():
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = DiscreteSpace(name="x5", lower_bound=2, upper_bound=3)
+    designspace = [x1, x2, x3, x4, x5]
+
+    doe = DoE(space=designspace)
+    assert doe.getDiscreteParameters() == [x2, x5]
+
+
+def test_get_categorical_parameters():
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = DiscreteSpace(name="x5", lower_bound=2, upper_bound=3)
+    designspace = [x1, x2, x3, x4, x5]
+
+    doe = DoE(space=designspace)
+    assert doe.getCategoricalParameters() == [x4]
+
+
+def test_addspace():
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = DiscreteSpace(name="x5", lower_bound=2, upper_bound=3)
+    designspace = [x1, x2, x3]
+
+    doe = DoE(space=designspace)
+    doe.addSpace(x4)
+    doe.addSpace(x5)
+
+    assert doe.space == [x1, x2, x3, x4, x5]
+
+
+if __name__ == "__main__":  # pragma: no cover
     pytest.main()
--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -1,29 +1,115 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 
 import numpy as np
 import pytest
-from f3dasm.sampling import randomuniform, latinhypercube, sobolsequence
-from f3dasm.src.designofexperiments import DesignOfExperiments
+from f3dasm.sampling.randomuniform import RandomUniform
+from f3dasm.sampling.latinhypercube import LatinHypercube
+from f3dasm.sampling.sobolsequence import SobolSequencing
 
-def test_randomuniform_samples():
-    ran = randomuniform.RandomUniform(DesignOfExperiments(), seed=42)
-    samples = ran.sample(numsamples=3, dimensions=2)
-    truth = np.array([[0.37454012, 0.95071431], [0.73199394, 0.59865848], [0.15601864, 0.15599452]])
-    assert samples == pytest.approx(truth)
+from f3dasm.src.designofexperiments import DoE
+from f3dasm.src.space import CategoricalSpace, ContinuousSpace, DiscreteSpace
 
-def test_latinhypercube_samples():
-    lhs = latinhypercube.LatinHypercube(DesignOfExperiments(), seed=42)
-    samples = lhs.sample(numsamples=3, dimensions=2)
+
+# Random Uniform Sampling
+
+
+def test_correct_randomuniform_sampling():
+    seed = 42
+
+    # Define the parameters
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = ContinuousSpace(name="x5", lower_bound=0.6, upper_bound=7.3)
+
+    # Create the design space
+    space = [x1, x2, x3, x4, x5]
+    design = DoE(space)
+
+    # Construct sampler
+    random_uniform = RandomUniform(doe=design, seed=seed)
+
+    numsamples = 5
+
+    ground_truth_samples = np.array(
+        [
+            [5.35886694, 362.04950766, 5.50435941],
+            [7.12940203, 67.77370256, 1.64516329],
+            [2.85886054, 330.74502678, 4.62747058],
+            [7.99377336, 17.62243824, 7.09839601],
+            [8.97629686, 88.62917268, 1.81822728],
+        ]
+    )
+    samples = random_uniform.sample(numsamples=numsamples, doe=design)
+
+    assert samples == pytest.approx(ground_truth_samples)
+
+
+def test_correct_latinhypercube_sampling():
+    seed = 42
+
+    # Define the parameters
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = ContinuousSpace(name="x5", lower_bound=0.6, upper_bound=7.3)
+
+    # Create the design space
+    space = [x1, x2, x3, x4, x5]
+    design = DoE(space)
+
+    # Construct sampler
+    random_uniform = LatinHypercube(doe=design, seed=seed)
+
+    numsamples = 5
+
+    ground_truth_samples = np.array(
+        [
+            [8.25875467, 95.61474051, 1.58087188],
+            [5.65177211, 222.26900536, 2.14903266],
+            [4.92588041, 80.40990153, 5.9196792],
+            [2.99177339, 233.70448765, 6.20364546],
+            [10.03525937, 321.96583454, 4.08549412],
+        ]
+    )
+    samples = random_uniform.sample(numsamples=numsamples, doe=design)
+    assert samples == pytest.approx(ground_truth_samples)
+
+
+def test_correct_sobolsequence_sampling():
+    seed = 42
+
+    # Define the parameters
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = ContinuousSpace(name="x5", lower_bound=0.6, upper_bound=7.3)
+
+    # Create the design space
+    space = [x1, x2, x3, x4, x5]
+    design = DoE(space)
+
+    # Construct sampler
+    sobol_sequencing = SobolSequencing(doe=design, seed=seed)
+
+    numsamples = 5
+
+    ground_truth_samples = np.array(
+        [
+            [2.4, 10.0, 0.6],
+            [6.35, 195.15, 3.95],
+            [8.325, 102.575, 2.275],
+            [4.375, 287.725, 5.625],
+            [5.3625, 148.8625, 4.7875],
+        ]
+    )
+    samples = sobol_sequencing.sample(numsamples=numsamples, doe=design)
     print(samples)
-    truth = np.array([[0.57733131, 0.71866484], [0.12484671, 0.53288616], [0.71867288, 0.31690477]])
-    assert samples == pytest.approx(truth)
+    assert samples == pytest.approx(ground_truth_samples)
 
-def test_sobolsequence_samples():
-    sob = sobolsequence.SobolSequencing(DesignOfExperiments(), seed=42)
-    samples = sob.sample(numsamples=3, dimensions=2)
-    print(samples)
-    truth = np.array([[0., 0.], [0.5, 0.5], [0.75, 0.25]])
-    assert samples == pytest.approx(truth)
 
-if __name__ == '__main__':
+if __name__ == "__main__":  # pragma: no cover
     pytest.main()
