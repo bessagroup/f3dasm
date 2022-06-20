09511fa741258d9de5990e94e966f534b38601d7
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2022-06-20 15:57:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.5'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    # 'requests', 'maya', 'records',
+     'numpy','pandas','matplotlib','SALib',
 ]
 
 # What packages are optional?
