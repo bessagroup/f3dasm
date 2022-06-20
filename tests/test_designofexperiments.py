bbccd1ac3c14ade08fa9e05a0bf5850afdf23b24
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2022-06-20 12:34:24 +0200

    added tests for doe.getNames()

--- tests/test_designofexperiments.py
+++ tests/test_designofexperiments.py
@@ -1,4 +1,3 @@
-from pandas import Categorical
 import pytest
 from f3dasm.src.designofexperiments import DoE
 from f3dasm.src.space import CategoricalSpace, ContinuousSpace, DiscreteSpace
@@ -54,6 +53,43 @@ def test_get_categorical_parameters():
 
     doe = DoE(space=designspace)
     assert doe.getCategoricalParameters() == [x4]
+
+
+def test_get_continuous_names():
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = DiscreteSpace(name="x5", lower_bound=2, upper_bound=3)
+    designspace = [x1, x2, x3, x4, x5]
+
+    doe = DoE(space=designspace)
+    assert doe.getContinuousNames() == ["x1", "x3"]
+
+
+def test_get_discrete_names():
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = DiscreteSpace(name="x5", lower_bound=2, upper_bound=3)
+    designspace = [x1, x2, x3, x4, x5]
+
+    doe = DoE(space=designspace)
+    assert doe.getDiscreteNames() == ["x2", "x5"]
+
+
+def test_get_categorical_names():
+    x1 = ContinuousSpace(name="x1", lower_bound=2.4, upper_bound=10.3)
+    x2 = DiscreteSpace(name="x2", lower_bound=5, upper_bound=80)
+    x3 = ContinuousSpace(name="x3", lower_bound=10.0, upper_bound=380.3)
+    x4 = CategoricalSpace(name="x4", categories=["test1", "test2", "test3"])
+    x5 = DiscreteSpace(name="x5", lower_bound=2, upper_bound=3)
+    x6 = CategoricalSpace(name="x6", categories=["material1", "material2", "material3"])
+    designspace = [x1, x2, x3, x4, x5, x6]
+
+    doe = DoE(space=designspace)
+    assert doe.getCategoricalNames() == ["x4", "x6"]
 
 
 def test_addspace():
