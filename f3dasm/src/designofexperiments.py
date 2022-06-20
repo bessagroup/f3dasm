a25d8734e9caa0a9a0d2225389387d716ed22574
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2022-06-20 12:25:41 +0200

    add docstrings

--- f3dasm/src/designofexperiments.py
+++ f3dasm/src/designofexperiments.py
@@ -1,4 +1,3 @@
-# %%
 from dataclasses import dataclass, field
 from typing import List
 
@@ -15,30 +14,67 @@ from f3dasm.src.space import (
 
 @dataclass
 class DoE:
+    """Design of experiments
+
+    Args:
+        space (list): list of parameters
+    """
 
     space: List[SpaceInterface] = field(default_factory=list)
 
     def addSpace(self, space: SpaceInterface) -> None:
+        """Add a new parameter to the searchspace
+
+        Args:
+            space (SpaceInterface): search space parameter to be added
+        """
         self.space.append(space)
         return
 
-    def getContinuousParameters(self):
+    def getContinuousParameters(self) -> list[ContinuousSpace]:
+        """Receive all the continuous parameters"""
         return [
             parameter
             for parameter in self.space
             if isinstance(parameter, ContinuousSpace)
         ]
 
-    def getDiscreteParameters(self):
+    def getContinuousNames(self) -> list[str]:
+        """Receive all the continuous parameter names"""
+        return [
+            parameter.name
+            for parameter in self.space
+            if isinstance(parameter, ContinuousSpace)
+        ]
+
+    def getDiscreteParameters(self) -> list[DiscreteSpace]:
+        """Receive all the discrete parameters"""
         return [
             parameter
             for parameter in self.space
             if isinstance(parameter, DiscreteSpace)
         ]
 
-    def getCategoricalParameters(self):
+    def getDiscreteNames(self) -> list[str]:
+        """Receive all the continuous parameter names"""
+        return [
+            parameter.name
+            for parameter in self.space
+            if isinstance(parameter, DiscreteSpace)
+        ]
+
+    def getCategoricalParameters(self) -> list[CategoricalSpace]:
+        """Receive all the categorical parameters"""
         return [
             parameter
+            for parameter in self.space
+            if isinstance(parameter, CategoricalSpace)
+        ]
+
+    def getCategoricalNames(self) -> list[str]:
+        """Receive all the continuous parameter names"""
+        return [
+            parameter.name
             for parameter in self.space
             if isinstance(parameter, CategoricalSpace)
         ]
