1c321cf1657095cb0456d3d970b2ab804264531e
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2022-06-28 16:30:27 +0200

    to v0.1.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/mpvanderschelling/testthings"
 EMAIL = "M.P.vanderSchelling@tudelft.nl"
 AUTHOR = "Martin van der Schelling"
 REQUIRES_PYTHON = ">=3.7.0, <4"
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
