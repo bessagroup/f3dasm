a771d4c18d900976200d8b7f9f00dbe70b2cdfcc
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2022-08-15 15:59:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ REQUIRES_PYTHON = ">=3.7.0, <4"
 # VERSION = get_version()
 
 # What packages are required for this module to be executed?
-REQUIRED = ["numpy", "scipy" "pandas", "matplotlib", "SALib", "hypothesis", "GPyOpt", "GPy"]
+REQUIRED = ["numpy", "scipy", "pandas", "matplotlib", "SALib", "hypothesis", "GPyOpt", "GPy"]
 
 # What packages are optional?
 EXTRAS = {
