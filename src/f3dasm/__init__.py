d6e37765fe3e3bce5cc5200b9039852d0981e38a
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2022-12-16 14:36:16 +0100

    v0.2.92

--- src/f3dasm/__init__.py
+++ src/f3dasm/__init__.py
@@ -5,7 +5,7 @@ F3DASM
 This is the initalizer of the F3DASM package
 """
 
-__version__ = '0.2.91'
+__version__ = '0.2.92'
 
 #                                                                       Modules
 # =============================================================================
