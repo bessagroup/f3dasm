1dfdf43fa4c4e9fac93f1ce18d4af5cdc5acb1f7
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2023-02-20 16:08:17 +0100

    changed version number to trigger workflows

--- src/f3dasm/__init__.py
+++ src/f3dasm/__init__.py
@@ -5,7 +5,7 @@ F3DASM
 This is the initalizer of the F3DASM package
 """
 
-__version__ = '0.2.95'
+__version__ = '0.2.96'
 
 #                                                                       Modules
 # =============================================================================
