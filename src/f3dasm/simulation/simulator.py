fec54f8863111dbbcc344282ad547d55bb9d9f93
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2023-05-26 15:49:03 +0200

    cleaned up Simulator base class

--- src/f3dasm/simulation/simulator.py
+++ src/f3dasm/simulation/simulator.py
@@ -17,12 +17,9 @@ __status__ = "Alpha"
 class Simulator:
     """Base class for a FEM simulator"""
 
-    # def __init__(self, data: Data):
-    #     self.data = data
-
     def pre_process(self) -> None:
         """Function that handles the pre-processing"""
-        pass
+        ...
         # raise NotImplementedError("No pre-process function implemented!")
 
     def execute(self) -> None:
@@ -31,8 +28,7 @@ class Simulator:
 
     def post_process(self) -> None:
         """Function that handles the post-processing"""
-        pass
-        # raise NotImplementedError("No post-process function implemented!")
+        ...
 
     def run(self) -> None:
         self.pre_process()
