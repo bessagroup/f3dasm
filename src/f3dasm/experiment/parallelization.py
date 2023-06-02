f936937fc08ce9a3ed1d0ddcafaa00f5fd16311c
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2023-06-02 15:02:44 +0200

    added run_operation_on_experiments

---
+++ src/f3dasm/experiment/parallelization.py
@@ -0,0 +1,61 @@
+#                                                                       Modules
+# =============================================================================
+
+
+# Standard
+from typing import Any, Callable, Dict, List
+
+# Third-party core
+from pathos.helpers import mp
+
+# Local
+from ..design import ExperimentData
+
+#                                                          Authorship & Credits
+# =============================================================================
+__author__ = 'Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)'
+__credits__ = ['Martin van der Schelling']
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
+
+
+def run_operation_on_experiments(data: ExperimentData, operation: Callable,
+                                 parallel: bool = False, **kwargs) -> List[Any]:
+    """Run an operation on a list of experiments
+
+    Parameters
+    ----------
+    data
+        ExperimtentData object
+    operation
+        Callable function that accepts one ExperimentData line
+    parallel, optional
+        Flag if the operation should be done in parallel, by default False
+
+    Returns
+    -------
+        List of returns from the operation
+    """
+
+    if parallel:
+        options = [
+            ({'index': index, 'value_input': value_input, 'value_output': value_output, **kwargs},)
+            for index, (value_input, value_output) in enumerate(data)
+        ]
+
+        def f(options: Dict[str, Any]) -> Any:
+            """This function wraps the operation to unpack the options dictionary"""
+            return operation(**options)
+
+        with mp.Pool() as pool:
+            # maybe implement pool.starmap_async ?
+            results = pool.starmap(f, options)
+
+    else:
+        results = []
+        for index, (value_input, value_output) in enumerate(data):
+            results.append(operation(index, value_input, value_output, **kwargs))
+
+    return results
