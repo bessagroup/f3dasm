a714e5ed20e0fb67bfac521e79061d031fb8331c
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2023-07-06 14:33:21 +0200

    back to -- convention

--- src/f3dasm/argparser.py
+++ src/f3dasm/argparser.py
@@ -1,21 +1,12 @@
 import argparse
 
-
-class DoublePlusPrefixAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, values)
-
-
 parser = argparse.ArgumentParser()
-
-# Argparse jobid if applicable
-
 
 # Create an ArgumentParser object
 parser = argparse.ArgumentParser()
 
 # Option with ++ prefix
-parser.add_argument('++jobid', type=int, action=DoublePlusPrefixAction, help='The PBS job ID for HPC', default=None)
+parser.add_argument('--jobid', type=int, help='The PBS job ID for HPC', default=None)
 
 # Parse the command-line arguments
 args = parser.parse_args()
