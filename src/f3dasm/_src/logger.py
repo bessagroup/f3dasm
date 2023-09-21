3cd4d1e85dcb6aa8d195ed0a35e458661600ca5e
Guillaume Broggi <25569517+GuillaumeBroggi@users.noreply.github.com>
2023-09-21 14:07:24 +0200

    added flexible logging #171

--- src/f3dasm/_src/logger.py
+++ src/f3dasm/_src/logger.py
@@ -9,8 +9,10 @@ import errno
 import logging
 import os
 from logging import FileHandler, StreamHandler
+from pathlib import Path
 from time import sleep
 from typing import TextIO
+import yaml
 
 if os.name == 'nt':
     import msvcrt
@@ -29,10 +31,6 @@ __status__ = 'Stable'
 # =============================================================================
 #
 # =============================================================================
-
-# Logging things
-logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
-logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
 
 class DistributedFileHandler(FileHandler):
@@ -68,7 +66,9 @@ class DistributedFileHandler(FileHandler):
             except IOError as e:
                 # the file is locked by another process
                 if e.errno == errno.EAGAIN:
-                    logger.debug("The log file is currently locked by another process. Retrying in 1 second...")
+                    logger.debug(
+                        "The log file is currently locked by another process. Retrying in 1 second..."
+                    )
                     sleep(1)
                 else:
                     logger.info(f"An unexpected IOError occurred: {e}")
@@ -107,19 +107,35 @@ def _unlock_file(file):
         fcntl.flock(file, fcntl.LOCK_UN)
 
 
-def _time_and_log(
-    func: Callable, logger=logging.Logger
-) -> Callable:
+def _time_and_log(func: Callable, logger=logging.Logger) -> Callable:
     @wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         start_time = perf_counter()
         value = func(*args, **kwargs)
-        logger.debug(f"Called {func.__name__} and time taken: {perf_counter() - start_time:.2f}s")
+        logger.debug(
+            f"Called {func.__name__} and time taken: {perf_counter() - start_time:.2f}s"
+        )
         return value
 
     return wrapper
 
+# f3dasm can be used as a library or as an application
+# If used as a library, the user is responsible to define a f3dasm logger
+# Otherwise, the default behavior is to load the f3dsam logging configuration
 
-# Create a logger
-logger = logging.getLogger("f3dasm")
+logger_name = "f3dsam"
+
+# Check for the existence of the f3dsam logger
+# This is required because getLogger() creates the logger if it does not exist
+# Loggers are stored in the logger manager (undocumented feature)
+if logger_name not in logging.Logger.manager.loggerDict.keys():
+    # If not, load the logging configuration that should create a f3dasm logger
+    config_path = Path(__file__).parent/"logger_config.yaml"
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+
+# Get existing logger without overwriting the existing configuration
+logger = logging.getLogger(name="f3dsam")
+
 time_and_log = partial(_time_and_log, logger=logger)
