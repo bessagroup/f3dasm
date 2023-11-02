0be8ddb83bcdbf67ecc51ba09b42c7280e0e9d77
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2023-11-02 17:55:46 -0400

    Add a method to ExperimentData that extracts all the ExperimentSamples in a list Fixes #201

--- src/f3dasm/_src/experimentdata/experimentdata.py
+++ src/f3dasm/_src/experimentdata/experimentdata.py
@@ -589,6 +589,27 @@ class ExperimentData:
                                 dict_output=self.output_data.get_data_dict(index), jobnumber=index,
                                 experimentdata_directory=self.path)
 
+    def get_experiment_samples(self, indices: Optional[Iterable[int]] = None) -> List[ExperimentSample]:
+        """
+        Gets the experiment_samples at the given indices.
+
+        Parameters
+        ----------
+        indices : Optional[Iterable[int]], optional
+            The indices of the experiment_samples to retrieve, by default None
+            If None, all experiment_samples are retrieved.
+
+        Returns
+        -------
+        List[ExperimentSample]
+            The ExperimentSamples at the given indices.
+        """
+        if indices is None:
+            # Return a list of the iterator over ExperimentData
+            return list(self)
+
+        return [self.get_experiment_sample(index) for index in indices]
+
     def _set_experiment_sample(self, experiment_sample: ExperimentSample) -> None:
         """
         Sets the ExperimentSample at the given index.
